27ff7aa53a952128e28237ad3cbad907c2802c23
MarcDagher <marcdagher47@gmail.com>
2024-09-11 11:09:09 +0300

    Adjusted SideBar UI

--- Agent_App/sidebar.py
+++ Agent_App/sidebar.py
@@ -12,14 +12,16 @@ def add_separator(color="#AAAAAA"):
   )
 
 def side_bar(session_state):
-
-  st.sidebar.markdown("""
-                      <strong style="font-size: 18px;">PersonaBot</strong> <span style="font-size: 15px;">is an agentic AI system designed to understand your personality then suggest career paths, with access to a Neo4j knowledge graph.</span>
+  st.sidebar.image("PersonaBot.png", output_format='PNG', use_column_width=True)
+  st.sidebar.markdown(""" 
+                      <p style="font-size: 15px;">
+                        An agentic AI system designed to test and analyze your personality. With access to a Neo4j knowledge graph, the system will suggest career paths suitable for your character.
+                      </p>
                       """, unsafe_allow_html=True)
 
   add_separator()
 
-  st.sidebar.button(label="Chat", on_click=set_current_page, kwargs={'page': 'chat'})
-  st.sidebar.button(label="Knowledge Graph", on_click=set_current_page, kwargs={'page': 'graph'})
+  st.sidebar.button(label="Chat", on_click=set_current_page, kwargs={'page': 'chat'}, use_container_width=True, type='primary')
+  st.sidebar.button(label="Knowledge Graph", on_click=set_current_page, kwargs={'page': 'graph'}, use_container_width=True)
 
   return session_state
